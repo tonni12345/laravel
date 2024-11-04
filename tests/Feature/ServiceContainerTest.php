20c03b3e9294d8f3ed73f9a909842e1767c57aab
Tonni Ramdani <tonniramdani@gmail.com>
2024-11-04 13:43:59 +0700

    nyoba instance

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -48,6 +48,21 @@ class ServiceContainerTest extends TestCase
         self::assertEquals("Tonni", $person1->firstName);
         self::assertEquals("Ramdani", $person2->lastName);
         self::assertSame($person1, $person2);
+    }
+
+    public function testInstance(){
+        $person = new Person("Tonni", "Ramdani");
+        $this->app->instance(Person::class, $person);
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals("Tonni", $person1->firstName);
+        self::assertEquals("Ramdani", $person2->lastName);
+        self::assertSame($person, $person1);
+        self::assertSame($person, $person2);
+        self::assertSame($person1, $person2);
+
 
     }
 }
